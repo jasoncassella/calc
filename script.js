08115b6adaf46e5d5afd4841922aa705d5022cb1
Jason Cassella <jasoncassella1@gmail.com>
2021-08-17 17:30:14 -0400

    added decimal button

--- script.js
+++ script.js
@@ -3,25 +3,24 @@ let buttons = document.querySelectorAll('.number');
 let operands = document.querySelectorAll('.operand');
 let clear = document.querySelector('#clear');
 let equals = document.querySelector('#equals');
+let decimal = document.querySelector('#decimal');
 
 let displayValue = 0;
 let operator;
 let num1;
 let num2;
-let num2total = 0;
 let final;
 
 buttons.forEach((number) => {
   number.addEventListener('click', (e) => {
-    if (displayValue != 0) {
-      // keep adding to the number
-      display.textContent = displayValue + e.target.textContent; //append the number to the textContent of the display
-      displayValue = display.textContent; // update the displayValue
-      num2 = parseInt(displayValue);
+    if (display.textContent !== '0') {
+      display.textContent += e.target.textContent; //append the number to the textContent of the display
+      displayValue = parseFloat(display.textContent); // update the displayValue
+      num2 = parseFloat(displayValue);
     } else {
       // if theres a 0 on the screen
       display.textContent = e.target.textContent; // textContent = number
-      displayValue = display.textContent; //update the displayValue
+      displayValue = parseFloat(display.textContent); //update the displayValue
     }
   });
 });
@@ -29,10 +28,10 @@ buttons.forEach((number) => {
 operands.forEach((operand) => {
   operand.addEventListener('click', (e) => {
     if (!displayValue) return; // if theres a 0 on the screen do nothing
-    num1 = parseInt(displayValue); // once you hit +,-,*, or /, the number before will be num1
+    num1 = displayValue; // once you hit +,-,*, or /, the number before will be num1
     operator = e.target.textContent; // operator = the operator button you hit
     display.textContent = 0;
-    displayValue = display.textContent;
+    displayValue = 0;
   });
 });
 
@@ -42,8 +41,12 @@ clear.addEventListener('click', () => {
 });
 
 equals.addEventListener('click', () => {
-  num2 = parseInt(displayValue);
+  num2 = displayValue;
   operate(operator, num1, num2);
+});
+
+decimal.addEventListener('click', (e) => {
+  display.textContent += e.target.textContent; //append the decimal to the textContent of the display
 });
 
 function add(a, b) {
